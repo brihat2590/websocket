b9e89031d92ad50a049edc5587410fc947662875
Brihat576 <thapabrihatbir321@gmail.com>
2024-11-27 08:33:49 +0530

    added the activity

--- index.js
+++ index.js
@@ -15,12 +15,28 @@ const io=new Server(expressServer,{
         origin:'*'
     }
 })
-io.on('connection',socket=>{
+io.on('connection',(socket)=>{
     console.log(`User ${socket.id} connected`)
+
+    //upon connection -only to user
+    socket.emit('message',"welcome to the chat app!")
+    //upon connection to all others
+    socket.broadcast.emit('message',`${socket.id.substring(0,5)} connected`)
+
+    //listening for a message event
     socket.on('message',data=>{
         console.log(data)
         
         io.emit('message',`${socket.id.substring(0,6)} : ${data}`)
+
+    })
+    socket.on('disconnect',()=>{
+        socket.broadcast.emit(`${socket.id.substring(0,5)} disconnected`)
+    })
+
+    //listen for activity
+    socket.on('activity',(name)=>{
+        socket.broadcast.emit(`activity event`,name)
 
     })
 })
--- public/app.js
+++ public/app.js
@@ -1,19 +1,28 @@
 const socket=io('ws://localhost:3000')
+const activity=document.querySelector('.activity')
+const msginput=document.querySelector('input')
 
 function sendMessage(e){
     e.preventDefault();
-    const input=document.querySelector('input')
-    if(input.value){
-        socket.emit('message',input.value);
-        input.value=''
+    
+    if(msginput.value){
+        socket.emit('message',msginput.value);
+        msginput.value=''
     }
-    input.focus()
+    msginput.focus()
 }
 document.querySelector('form')
     .addEventListener('submit',sendMessage)
 
 socket.on('message',(data)=>{
+    activity.textContent=''
     const li=document.createElement('li')
     li.textContent=data;
     document.querySelector('ul').appendChild(li)
+})
+msginput.addEventListener("keypress",()=>{
+    socket.emit('activity',socket.emit(socket.id.substring(0,5)))
+})
+socket.on('activity',(name)=>{
+    activity.textContent=`${name} is typing..`
 })
